e6ba7334a46d2ca6624155c2e9732b44504597b0
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-05 16:19:08 -0300

    [feat] - transformando http em websocket e conectando com frontend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path"
+import http from "http";
+import {Server} from "socket.io";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -10,7 +12,14 @@ const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 
 app.use(express.static(diretorioPublico));
 
-app.listen(port, () => {
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(port, () => {
     console.log("Servidor on.");
 })
 
+const io = new Server(servidorHttp);
+
+io.on("connection", () => {
+    console.log("cliente se conectou");
+});
