1d0ce469276f61a00b765bc4704ffeb8289dde2e
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-09 00:57:21 -0300

    [feat] - emitindo primeiro evento no frontend e recebendo no backend, printando o texto digitado na tela

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,8 @@ import io from "./server.js";
 
 io.on("connection", (socket) => {
     console.log("Cliente conectado, IP " + socket.id);
+
+    socket.on("texto_editor", (texto) => {
+        console.log(texto);
+    })
 });
