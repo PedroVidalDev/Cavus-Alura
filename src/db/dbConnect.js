566d3ae33385bf5ce2f0bf4bcea6755d3dccc307
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-20 18:21:39 -0300

    feat: criando a colecao de usuarios no db

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -3,15 +3,17 @@ import { MongoClient } from "mongodb";
 const cliente = new MongoClient("mongodb+srv://root:root@cluster0.7lwongi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 let documentosColecao;
+let usuariosColecao;
 
 try{
     await cliente.connect();
 
     const db = cliente.db("alura-websockets");
     documentosColecao = db.collection("documentos");
+    usuariosColecao = db.collection("usuarios");
 
 } catch(erro){
     console.log(erro);
 }
 
-export {documentosColecao};
+export {documentosColecao, usuariosColecao};
